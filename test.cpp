7ad501a0a674e8f99d573ac5250dea01a083da31
Muhammad Ghazwan Khariz <2500018019@webmail.uad.ac.id>
2025-11-05 03:30:59 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int total, hours, minutes, seconds;
+
+    cout << "Input bilangan >1 (satuan detik): ";
+    cin >> total;
+
+    hours = total / 3600;              
+    
+    minutes = (total % 3600) / 60;     
+    
+    // sisa detik terakhir
+    seconds = total % 60;              
+
+    cout << "\nOutput Konversi:" << endl;
+    cout << total << " detik = " 
+         << hours << " Jam, " 
+         << minutes << " Menit, " 
+         << seconds << " Detik." << endl;
+
     return 0;
 }
