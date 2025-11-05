77086b003059f6d1713113dcc51177ae96c39f1a
Ghazwan <kharizghazwan@gmail.com>
2025-11-05 09:52:15 +0700

    .

--- test.cpp
+++ test.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int totalDetik, jam, menit, detik;
+
+    cout << "Masukkan bilangan >1 (satuan detik): ";
+    cin >> totalDetik;
+
+    jam = totalDetik / 3600;              // 1 jam = 3600 detik
+    menit = (totalDetik % 3600) / 60;     // sisa detik dibagi 60 untuk dapat menit
+    detik = totalDetik % 60;              // sisa detik terakhir
+
+    cout << "\nHasil Konversi:" << endl;
+    cout << totalDetik << " detik = " 
+         << jam << " Jam, " 
+         << menit << " Menit, " 
+         << detik << " Detik." << endl;
+
     return 0;
 }
